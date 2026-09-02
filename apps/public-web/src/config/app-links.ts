0ab3cc82acb79c728d53a3603f1048911647052f
David AILI <102434297+ailidmx@users.noreply.github.com>
2026-09-01 18:25:12 -0600

    fix(public): add apps/public-web/src/config/app-links.ts

---
+++ apps/public-web/src/config/app-links.ts
@@ -0,0 +1,2 @@
+export const PLAYER_APP_URL = process.env.NEXT_PUBLIC_PRIVATE_APP_URL ?? "https://play.docfoot.fr";
+export const ADMIN_APP_URL = process.env.NEXT_PUBLIC_ADMIN_APP_URL ?? "https://admin.docfoot.fr";
